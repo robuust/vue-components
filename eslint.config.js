9e511ca6b3bd6b73f42ec436f3843a19b0404ca2
Rutger Bakker <rutger7bakker@gmail.com>
2024-11-11 14:41:36 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,7 @@ export default [
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/node_modules/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/node_modules/**', '**/docs/.vitepress/**'],
   },
 
   js.configs.recommended,
